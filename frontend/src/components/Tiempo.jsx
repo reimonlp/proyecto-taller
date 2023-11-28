5d8ea695f4fe12027e7dae9692da0e94a29f3d7a
reimon <ligorria.reimon@gmail.com>
2023-11-28 06:43:57 -0300

    Refactor time calculation in Tiempo component

--- frontend/src/components/Tiempo.jsx
+++ frontend/src/components/Tiempo.jsx
@@ -22,24 +22,25 @@ moment.updateLocale('es', {
 const Tiempo = ( {ts} ) => {
   const [timeDiff, setTimeDiff] = useState('')
 
+  // useMemo permite ejecutar una función solo cuando cambia una variable
   useMemo(() => {
     // Date y moment son librerías para trabajar con fechas
     // Date convierte el timestamp en una fecha
     // Moment permite mostrar el tiempo transcurrido de una fecha
 
-    const theDate = new Date(ts)   // Obtiene la fecha del timestamp
-    const mom = moment(theDate)    // Crea un objeto moment con la fecha
+    const theDate = new Date(ts)
+    const mom = moment(theDate)
 
     // Actualiza el tiempo transcurrido cada minuto
     const interval = setInterval(() => {
       setTimeDiff(mom.fromNow())
     }, 60 * 1000)
 
-    // Actualiza el tiempo transcurrido por primera vez
-    setTimeDiff(mom.fromNow())
 
-    // Elimina el intervalo cuando se desmonta el componente
-    return () => clearInterval(interval)
+    setTimeDiff(mom.fromNow())    // Actualiza el tiempo transcurrido por primera vez
+
+
+    return () => clearInterval(interval)    // Elimina el intervalo cuando se desmonta el componente
   }, [ts])
 
   return (
